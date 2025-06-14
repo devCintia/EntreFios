668aaced46f77b78492acdbf5d94f331697ec7ec
Cintia <cintia.dev2024@gmail.com>
2025-06-13 22:22:31 -0300

    Implantação do JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 /*
-  O que precisamos fazer? - Quando o usuário clicar no botão "Aplicar filtros", vamos filtrar as cartas baseado na categoria e no preço máximo selecionados
-    OBJETIVO 1 - Criar a funcionalidade de filtrar as cartas
+  O que precisamos fazer? - Quando o usuário clicar no botão "Aplicar filtros", vamos filtrar as bolsas baseado na categoria e no preço máximo selecionados
+    OBJETIVO 1 - Criar a funcionalidade de filtrar as bolsas
         passo 1 - pegar o botao de aplicar filtros  do HTML e mandar pro JS
         passo 2 - escutar o clique no botão de aplicar filtros
         passo 3 - pegar os valores dos campos de categoria e preço
-        passo 4 - para cada carta, verificar se ela deve ser mostrada ou escondida 
+        passo 4 - para cada bolsa, verificar se ela deve ser mostrada ou escondida 
 */
 
 const botaoFiltrar = document.querySelector('.btn-filtrar');
@@ -14,33 +14,33 @@ botaoFiltrar.addEventListener('click', () => {
     console.log(categoriaSelecionada);
     const precoMaximoSelecionado = document.querySelector('#preco').value;
     console.log(precoMaximoSelecionado);
-    const cartas = document.querySelectorAll('.carta');
-    cartas.forEach((carta) => {
-        const categoriaCarta = carta.dataset.categoria;
-        const precoCarta = carta.dataset.preco;
+    const bolsas = document.querySelectorAll('.bolsa');
+    bolsas.forEach((bolsa) => {
+        const categoriaBolsa = bolsa.dataset.categoria;
+        const precoBolsa = bolsa.dataset.preco;
 
-        let mostrarCarta = true;
+        let mostrarBolsa = true;
 
         const temFiltroDeCategoria = categoriaSelecionada !== '';
-        const cartaNaoBateComFiltroDeCategoria = categoriaSelecionada.toLowerCase() !== categoriaCarta.toLowerCase();
+        const bolsaNaoBateComFiltroDeCategoria = categoriaSelecionada.toLowerCase() !== categoriaBolsa.toLowerCase();
 
-        if (temFiltroDeCategoria && cartaNaoBateComFiltroDeCategoria) {
-            mostrarCarta = false;
+        if (temFiltroDeCategoria && bolsaNaoBateComFiltroDeCategoria) {
+            mostrarBolsa = false;
         }
 
         const temFiltroDePreco = precoMaximoSelecionado !== '';
-        const cartaNaoBateComFiltroDePrecoMaximo = parseFloat(precoCarta) > parseFloat(precoMaximoSelecionado);
+        const bolsaNaoBateComFiltroDePrecoMaximo = parseFloat(precoBolsa) > parseFloat(precoMaximoSelecionado);
 
-        if (temFiltroDePreco && cartaNaoBateComFiltroDePrecoMaximo) {
-            mostrarCarta = false;
+        if (temFiltroDePreco && bolsaNaoBateComFiltroDePrecoMaximo) {
+            mostrarBolsa = false;
         }
 
-        if (mostrarCarta) {
-            carta.classList.add('mostrar');
-            carta.classList.remove('esconder');
+        if (mostrarBolsa) {
+            bolsa.classList.add('mostrar');
+            bolsa.classList.remove('esconder');
         } else {
-            carta.classList.remove('mostrar');
-            carta.classList.add('esconder');
+            bolsa.classList.remove('mostrar');
+            bolsa.classList.add('esconder');
         }
     });
 });
